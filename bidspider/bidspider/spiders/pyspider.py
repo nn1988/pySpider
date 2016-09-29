23862c49b3b8591b6a2e10b8dbb0a0f2f538f388
nn1988 <13120090@bjtu.edu.cn>
2016-09-29 13:44:31 +0800

    edition2

--- bidspider/bidspider/spiders/pyspider.py
+++ bidspider/bidspider/spiders/pyspider.py
@@ -1,23 +1,48 @@
 # import os
 # import re
 import scrapy
-import urllib2
+# import urllib2
 
 from scrapy.selector import Selector
-# from scrapy.item import spiderItem
+from bidspider.items import BidspiderItem
 # from scrapy.http import Request
 
 class bidSpider(scrapy.Spider):
     name                = "BidSpider"
     allowed_domains     = ["www.chinabidding.com.cn"]
-    start_urls          = ["https://www.chinabidding.cn/search/searchgj/zbcg?rp=30&categoryid=1%2C2&keywords=%E5%9F%8E%E5%B8%82%E8%BD%A8%E9%81%93&page=1&areaid=5%2C6%2C&table_type=1%2C2%2C4%2C5%2C&search_type=TITLE&b_date=year"]
+    start_urls          = ["https://www.chinabidding.cn/search/searchzbw/search2?keywords=%E5%9F%8E%E5%B8%82%E8%BD%A8%E9%81%93&table_type=&areaid=&categoryid=1&b_date=year",
+                           "https://www.chinabidding.cn/search/searchzbw/search2?rp=22&categoryid=1&keywords=%E5%9F%8E%E5%B8%82%E8%BD%A8%E9%81%93&page=2&areaid=&table_type=0&b_date=year"]
 
     def parse(self, response):
-        # sel             = Selector(response)
-        # postion         = sel.xpath('/html/body/div[1]/div[1]/div[12]/table/tbody').extract()
-        # _url            = postion.xpath('//href')
+        sel             = Selector(response)
+        strXpathPre     = '//*[@id="cTable"]/tbody/tr/td[1]/table/tbody/tr/td/table/tbody/tr['
+        strXpathEx      = ']/td[2]/a/'
+        BEGINORDER      = 2
+        ENDORDER        = 44
+        items           = []
+        filetag         = open('x.txt','w')
+        while BEGINORDER <= ENDORDER:
+            item                 = BidspiderItem()
+            strXpath             = strXpathPre + str(BEGINORDER) + strXpathEx
+            urlTrans             = sel.xpath( strXpath + '@href' ).extract()
+            titleTrans           = sel.xpath( strXpath + 'text()' ).extract()
+            item['sigUrl']       = 'www.chinabidding.cn/' + urlTrans[0].encode('utf-8')
+            item['bidTitle']     = titleTrans[0]
+            BEGINORDER += 2
+            items.append(item)
 
-        res = response.body
-        FileTag = open(filename, 'w')
-        FileTag.write(res)
+        for i in items:
+            filetag.write(i['bidTitle'].encode('utf-8') + ',')
+            filetag.write(i['sigUrl'].encode('utf-8') + '\n')
+
+        filetag.close()
+        # filetag.write(items)
+
+        '''
+        https://www.chinabidding.cn/search/searchzbw/search2?keywords=%E5%9F%8E%E5%B8%82%E8%BD%A8%E9%81%93&table_type=&areaid=&categoryid=1&b_date=year
+        https://www.chinabidding.cn/search/searchzbw/search2?rp=22&categoryid=1&keywords=%E5%9F%8E%E5%B8%82%E8%BD%A8%E9%81%93&page=2&areaid=&table_type=0&b_date=year
+        https://www.chinabidding.cn/search/searchzbw/search2?rp=22&categoryid=1&keywords=%E5%9F%8E%E5%B8%82%E8%BD%A8%E9%81%93&page=3&areaid=&table_type=0&b_date=year
+        '''
+    # def parse_
+
 
